f5e1f65344e300b8c4898bafc7d8f08a34d5245a
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-29 22:27:08 +0700

    setting up connection mongoose and nodemon

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB")
+    .then(() => console.log('Connected to MongoDB!'));
 
 app.listen(PORT, function(){
-    console.log("Server started on port" + PORT);
+    console.log(`Server started on port ${PORT}`);
 });
