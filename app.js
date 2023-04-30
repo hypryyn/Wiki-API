4e77cb47fdd58413d5c5f5f762d408fc6f83dee3
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-30 19:29:24 +0700

    GET

--- app.js
+++ app.js
@@ -13,6 +13,23 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://127.0.0.1:27017/wikiDB")
     .then(() => console.log('Connected to MongoDB!'));
 
+const articleSchema = {
+    title: String,
+    content: String
+}
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", async(req,res)=>{
+    try{
+    const foundArticles= await Article.find();
+        // console.log(foundArticles);
+        res.send(foundArticles);
+    }catch(err){
+        // console.log(err);
+        res.send(err);
+    }
+});
+
 app.listen(PORT, function(){
     console.log(`Server started on port ${PORT}`);
 });
